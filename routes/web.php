c2691a8c5aa16505a16197d5ec4b91d2f186dac0
Yohanes Cahyadi <yohanescahyadi59@gmail.com>
2023-04-08 00:18:32 +0700

    Create routes for categories and products

--- routes/web.php
+++ routes/web.php
@@ -24,14 +24,23 @@ Route::prefix('admin')->group(function () {
     });
 
     Route::controller(CategoryController::class)->prefix('/category')->group(function () {
-        Route::get('', 'index')->name('beranda.category');
+        Route::get('', 'index')->name('category.beranda');
+        Route::get('tambah', 'create')->name('category.tambah');
+        Route::post('tambah', 'store')->name('category.tambah.simpan');
+        Route::get('edit/{id}', 'edit')->name('category.edit');
+        Route::post('edit/{id}', 'update')->name('category.edit.update');
+        Route::get('hapus/{id}', 'destroy')->name('category.hapus');
     });
 
     Route::controller(ProductController::class)->prefix('/product')->group(function () {
-        Route::get('', 'index')->name('beranda.product');
+        Route::get('', 'index')->name('product.beranda');
+        Route::get('tambah', 'create')->name('product.tambah');
+        Route::post('tambah', 'store')->name('product.tambah.simpan');
+        Route::get('edit/{id}', 'edit')->name('product.edit');
+        Route::post('edit/{id}', 'update')->name('product.edit.update');
+        Route::get('hapus/{id}', 'destroy')->name('product.hapus');
     });
 });
-
 
 // Kasir
 Route::prefix('/kasir')->group(function () {
