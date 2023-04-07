ed17587025d56c74de12e0a59a2ce9674c8d1614
Yohanes Cahyadi <yohanescahyadi59@gmail.com>
2023-04-07 22:21:58 +0700

    add relation to cart in User Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,4 +48,9 @@ class User extends Authenticatable
     {
         return $this->belongsTo(Role::class, 'role_id');
     }
+
+    public function cart()
+    {
+        return $this->hasOne(Cart::class);
+    }
 }
