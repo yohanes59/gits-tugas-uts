6e0b5c9c69e7655c29000640eb0d99535cd03ea5
Yohanes Cahyadi <yohanescahyadi59@gmail.com>
2023-04-07 22:16:59 +0700

    create Role Model

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -2,10 +2,14 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Role extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+    ];
 }
