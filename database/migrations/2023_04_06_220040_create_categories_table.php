d62e17f87bc948d19c56074497edba750c04b4ee
Yohanes Cahyadi <yohanescahyadi59@gmail.com>
2023-04-08 05:58:12 +0700

    create category migration

--- database/migrations/2023_04_06_220040_create_categories_table.php
+++ database/migrations/2023_04_06_220040_create_categories_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('image', 255)->nullable();
+            $table->string('name', 50);
             $table->timestamps();
         });
     }
