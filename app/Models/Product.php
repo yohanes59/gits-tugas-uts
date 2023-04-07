27293f894c0cb176ebfd2dc5aec015a79e879e50
Yohanes Cahyadi <yohanescahyadi59@gmail.com>
2023-04-07 22:20:46 +0700

    create Product Model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,10 +2,23 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Product extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'category_id',
+        'name',
+        'price',
+        'image',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'category_id');
+    }
 }
