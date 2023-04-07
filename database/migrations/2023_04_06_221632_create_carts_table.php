3baaa2451d1365c0fe415aa526a2d46840c2fefc
Yohanes Cahyadi <yohanescahyadi59@gmail.com>
2023-04-08 05:58:49 +0700

    create cart migration

--- database/migrations/2023_04_06_221632_create_carts_table.php
+++ database/migrations/2023_04_06_221632_create_carts_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('carts', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->nullOnDelete();
+            $table->unsignedInteger('total')->default(0);
             $table->timestamps();
         });
     }
