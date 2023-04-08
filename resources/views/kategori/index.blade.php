ce836152afdcb55988b54dc35a0e39723f4b3741
fadlunmadihij <fadlunmadihij@gmail.com>
2023-04-08 10:58:24 +0700

    beranda kategori

---
+++ resources/views/kategori/index.blade.php
@@ -0,0 +1,21 @@
+<table class="table-warning">
+  <thead>
+    <tr>
+      <th scope="col">No</th>
+      <th scope="col">Name</th>
+      <th scope="col">Image</th>
+      <th scope="col">Action</th>
+    </tr>
+  </thead>
+  <tbody> 
+    <tr>
+      <th scope="row">1</th>
+      <td>Name</td>
+      <td>Image</td>
+      <td>
+        <a href="" class="btn btn-primary">Hapus</a>
+        <a href="" class="btn btn-danger">Edit</a>
+      </td>
+    </tr>
+  </tbody>
+</table>
