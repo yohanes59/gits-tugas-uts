c057444fd78362a056175d30d18d9fa83f68b947
awaqo <aqiljawadalfurqon@gmail.com>
2023-04-08 15:16:45 +0700

    add auth layout, add register view

--- resources/views/include/style.blade.php
+++ resources/views/include/style.blade.php
@@ -4,5 +4,8 @@
 <!-- Font Awesome -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
 
+<!-- Bootstrap Icons -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+
 {{-- sb admin css --}}
 <link rel="stylesheet" href="{{ asset('sb-admin/css/sb-admin-2.css') }}">
