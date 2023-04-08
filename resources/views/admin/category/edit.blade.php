8471488083aee55c2a6c659657cd9f10d7cd6fdc
Yohanes Cahyadi <yohanescahyadi59@gmail.com>
2023-04-09 04:35:59 +0700

    feat: integrate the controller with edit category view

--- resources/views/admin/category/edit.blade.php
+++ resources/views/admin/category/edit.blade.php
@@ -1,20 +1,36 @@
-<form action="/kategori" method="POST" enctype="multipart/form-data">
-    @csrf
-    @method('put')
-    <div class="mb-3"> 
-      <label for="exampleInpuName" class="form-label">Edit Kategori</label>
-      <input type="text"  class="form-control @error('name') is-invalid @enderror" name="name">
-      @error('name')
-      <div class="invalid-feedback">{{ $message }}</div>
-      @enderror
-    </div>
-    <div class="mb-3">
-      <label for="exampleInpuFoto" class="form-label">Foto</label>
-      <input type="file"  class="form-control" name="foto"> <br>
-      <img src="" width="100px" alt="">
-      @error('foto')
-      <div class="invalid-feedback">{{ $message }}</div>
-      @enderror
-    </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
-  </form>
+@extends('layouts.app')
+
+@section('title', 'Category - Edit')
+
+@section('content')
+    <form action="{{ url('admin/category/' . $kategori->id) }}" method="POST" enctype="multipart/form-data">
+        @method('PUT')
+        @csrf
+        <div class="my-2">
+            <label for="nama_kategori" class="form-label">Tambah Kategori</label>
+            <input type="text" value="{{ $kategori->name }}" id="nama_kategori" name="nama_kategori"
+                class="form-control @error('nama_kategori') is-invalid @enderror">
+            @error('nama_kategori')
+                <div class="invalid-feedback">{{ $message }}</div>
+            @enderror
+        </div>
+
+        <div>
+            @if (isset($kategori) && $kategori->image)
+                <img id="image-preview" src="{{ asset('storage/images/' . $kategori->image) }}" width="160px"
+                    height="150px">
+            @else
+                <img id="image-preview">
+            @endif
+        </div>
+
+        <div class="my-2">
+            <label for="gambar" class="form-label">Image</label>
+            <input type="file" id="gambar" class="form-control" name="gambar" onchange="previewImage()">
+            @error('gambar')
+                <div class="invalid-feedback">{{ $message }}</div>
+            @enderror
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+@endsection
