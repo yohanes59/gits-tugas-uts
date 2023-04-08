fcf71cae4a642118d1ab2b3e64bd0a023c20633e
Yohanes Cahyadi <yohanescahyadi59@gmail.com>
2023-04-08 21:17:33 +0700

    create index category page

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -1,28 +1,48 @@
 @extends('layouts.app')
 
-@section('title', 'Beranda - Category')
+@section('title', 'Category - Beranda')
 
 @section('content')
-    <div>Beranda Category</div>
-    <table class="table-warning">
+    {{-- {{ $kategori }} --}}
+    <h1>Beranda Category</h1>
+    <a href="{{ url('/admin/category/create') }}" class="btn btn-primary my-3">Tambah Data</a>
+
+    <table class="table table-warning">
         <thead>
-          <tr>
-            <th scope="col">No</th>
-            <th scope="col">Name</th>
-            <th scope="col">Image</th>
-            <th scope="col">Action</th>
-          </tr>
+            <tr>
+                <th scope="col">No</th>
+                <th scope="col">Name</th>
+                <th scope="col">Image</th>
+                <th scope="col">Dibuat pada</th>
+                <th scope="col">Action</th>
+            </tr>
         </thead>
-        <tbody> 
-          <tr>
-            <th scope="row">1</th>
-            <td>Name</td>
-            <td>Image</td>
-            <td>
-              <a href="" class="btn btn-primary">Hapus</a>
-              <a href="" class="btn btn-danger">Edit</a>
-            </td>
-          </tr>
+        <tbody>
+            @foreach ($kategori as $item)
+                <tr>
+                    <th scope="row">{{ $loop->iteration }}</th>
+                    <td>{{ $item->name }}</td>
+                    <td>
+                        @if ($item->image != '')
+                            <img src="{{ $item->image }}" alt="gambar kategori {{ $item->name }}" width="60"
+                                height="60">
+                        @else
+                            <img src="{{ asset('img/no-image.jpg') }}" alt="gambar kategori {{ $item->name }}"
+                                width="60" height="60">
+                        @endif
+                    </td>
+                    <td>{{ $item->created_at->format('d M Y H:i:s') }}</td>
+                    <td class="d-flex gap-2">
+                        <a href="/admin/category/{{ $item->id }}/edit" class="btn btn-primary">Edit</a>
+                        <form action="{{ url('/admin/category/' . $item->id) }}" method="POST">
+                            @method('DELETE')
+                            @csrf
+                            <button class="btn btn-danger"
+                                onclick="return confirm('Yakin Ingin Menghapus Data Ini?')">Hapus</button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
         </tbody>
-      </table>
+    </table>
 @endsection
