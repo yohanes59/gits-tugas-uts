4401c1622b0e527db31b49ad51b8bb96044c6af6
Yohanes Cahyadi <yohanescahyadi59@gmail.com>
2023-07-14 03:57:32 +0700

    fix error paymentCalculate js load in admin page

--- resources/views/include/script.blade.php
+++ resources/views/include/script.blade.php
@@ -1,7 +1,9 @@
     <!-- Bootstrap core JavaScript-->
     <script src="{{ asset('sb-admin/js/jquery.min.js') }}"></script>
     <script src="{{ asset('sb-admin/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous">
+    </script>
 
     <!-- Core plugin JavaScript-->
     <script src="{{ asset('sb-admin/js/jquery.easing.min.js') }}"></script>
@@ -11,4 +13,7 @@
 
     <script src="{{ asset('assets/js/imgPreview.js') }}"></script>
     <script src="{{ asset('assets/js/spinner.js') }}"></script>
-    <script src="{{ asset('assets/js/paymentCalculate.js') }}"></script>
+
+    @if (request()->is('cashier/*'))
+        <script src="{{ asset('assets/js/paymentCalculate.js') }}"></script>
+    @endif
