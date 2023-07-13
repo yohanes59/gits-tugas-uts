2e3b8cafc0b4499f58f3d4f5eaf0612411a55e88
Yohanes Cahyadi <yohanescahyadi59@gmail.com>
2023-07-14 05:49:24 +0700

    feat: add total income daily to yearly in admin

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -12,41 +12,52 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        $data['category'] = Category::count(); 
+        $currentDay = date('d');
+        $currentWeek = date('W');
+        $currentMonth = date('m');
+        $currentYear = date('Y');
+
+        $data['category'] = Category::count();
         $data['product'] = Product::count();
         $data['transaction'] = Transaction::count();
         $data['cashier'] = User::where('name', '!=', 'admin')->get();
+
+        $data['daily'] = Transaction::whereDay('created_at', $currentDay)->sum('grandtotal');
+        $data['weekly'] = Transaction::whereRaw('WEEK(created_at) = ?', [$currentWeek])->sum('grandtotal');
+        $data['monthly'] = Transaction::whereMonth('created_at', $currentMonth)->sum('grandtotal');
+        $data['yearly'] = Transaction::whereYear('created_at', $currentYear)->sum('grandtotal');
         return view('admin.dashboard.index', $data);
     }
 
-    public function account() {
+    public function account()
+    {
         $kasir = User::where('name', '!=', 'admin')->get();
         return view('admin.account.index', ['kasir' => $kasir]);
     }
 
     public function edit($id)
-	{
-		$user = User::findOrFail($id);
-		
-		return view('admin.account.edit', ['users' => $user]);
-	}
+    {
+        $user = User::findOrFail($id);
+
+        return view('admin.account.edit', ['users' => $user]);
+    }
 
     public function update(Request $request, $id)
-	{
-        
-		$user = [
-			'name' => $request->name
-		];
+    {
 
-		User::find($id)->update($user);
+        $user = [
+            'name' => $request->name
+        ];
 
-		return redirect('/admin/cashier-account/edit/'.$id)->with('alert','Berhasil Update data');
-	}
+        User::find($id)->update($user);
+
+        return redirect('/admin/cashier-account/edit/' . $id)->with('alert', 'Berhasil Update data');
+    }
 
     public function destroy($id)
-	{
-		$user = User::findOrFail($id);
+    {
+        $user = User::findOrFail($id);
         $user->delete();
         return redirect('/admin/cashier-account')->with('alert', 'Berhasil menghapus akun');
-	}
+    }
 }
--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -73,4 +73,77 @@
             </div>
         </div>
     </div>
+    <div class="row">
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-info shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">
+                                Pemasukan Harian</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">Rp {{ number_format($daily, 0, ',', '.') }}
+                            </div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fa-solid fa-2x text-gray-300 fa-calendar"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-info shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">
+                                Pemasukan Mingguan</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">Rp {{ number_format($weekly, 0, ',', '.') }}
+                            </div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fas fa-calendar-week fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-info shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">
+                                Pemasukan Bulanan</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">Rp
+                                {{ number_format($monthly, 0, ',', '.') }}</div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fas fa-calendar-days fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-info shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">
+                                Pemasukan Tahunan</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">Rp {{ number_format($yearly, 0, ',', '.') }}
+                            </div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fas fa-money-bills fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
