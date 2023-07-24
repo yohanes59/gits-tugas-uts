31e82b41d647bdaf5527ef03117f808c19df1671
Yohanes Cahyadi <yohanescahyadi59@gmail.com>
2023-07-24 19:51:44 +0700

    fix: title name in auth blade

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="shortcut icon" href="{{ asset('img/coffee.png') }}" type="image/x-icon">
-    <title>@yield('title') || Coffee Shop UTS</title>
+    <title>@yield('title') || Cafe XYZ</title>
 
     @include('include.style')
 </head>
